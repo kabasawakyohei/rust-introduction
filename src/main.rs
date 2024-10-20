1e49c89abd44c4ce1e0bc13a57e8225c2f9aa44d
kyohei kabasawa <slifekb2@gmail.com>
2024-10-21 08:24:53 +0900

    practice test

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,18 @@ fn func_ex_div_some(x: i32, y: i32) -> Option<i32> {
     ans
 }
 
+fn add_i32(x: i32, y: i32) -> i32 {
+    x + y
+}
+
+#[test]
+fn test1(){
+    assert_eq!(add_i32(10, 23), 33)
+}
+
 
 fn main() {
-    println!("Hello, world!"); 
+    println!("Hello, world!");
+    println!("{:?}",func_ex_div_some(20, 10));
+    println!("{}", add_i32(22, 11));
 }
